d539181466ca9d274ff3fa984b5af05ec6a2ee18
Jorge Estébenez <jorgecodesync@gmail.com>
2024-10-20 21:01:21 +0200

    update profile image and curriculum url

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,7 +1,5 @@
-import IconDribbble from "../components/icons/dribbble.astro";
 import IconGithub from "../components/icons/github.astro";
 import IconLinkedin from "../components/icons/linkedin.astro";
-import IconTwitter from "../components/icons/twitter.astro";
 
 export const SOCIALS = [
   {
